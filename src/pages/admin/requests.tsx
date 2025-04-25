bba985f2b173313afac343f832a01d08be7b3bef
Jirawat Chanasit <jirawat.chn@spumail.net>
2025-04-25 18:58:26 +0700

    update readme

--- src/pages/admin/requests.tsx
+++ src/pages/admin/requests.tsx
@@ -134,7 +134,10 @@ export const AdminRequestsPage = ({
                 }[statusText];
 
                 return (
-                  <tr key={data.id} className="h-12 border-b border-slate-300">
+                  <tr
+                    key={i}
+                    className="h-12 border-b border-slate-300 even:bg-slate-100 odd:bg-slate-200 transition-colors"
+                  >
                     <td className="px-4 py-2 text-center">{i + 1}</td>
                     <td className="px-4 py-2 text-left">{data.senderName}</td>
                     <td className="px-4 py-2 text-left">{data.department}</td>
--- src/pages/user/request.tsx
+++ src/pages/user/request.tsx
@@ -107,7 +107,10 @@ export const UserMyRequestsPage = ({
               }[statusText];
 
               return (
-                <tr key={i} className="h-12 border-b border-slate-300">
+                <tr
+                  key={i}
+                  className="h-12 border-b border-slate-300 even:bg-slate-100 odd:bg-slate-200 transition-colors"
+                >
                   <td className="px-4 py-2 text-center">{i + 1}</td>
                   <td className="px-4 py-2">{data.courseName}</td>
                   <td className="px-4 py-2 text-right">
